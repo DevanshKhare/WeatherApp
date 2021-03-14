69f3155873fbd96373e8b8e18ee5005736a02357
DevanshKhare <devanshkhare@gmail.com>
2021-03-14 08:15:20 +0530

    script for heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 var requests = require('requests');
+const port = process.env.PORT || 8000;
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
 const replaceVal = (tempval, orgval) => {
@@ -37,4 +38,4 @@ const server = http.createServer((req, res) => {
     }
 
 
-}).listen(8000, "127.0.0.1");
+}).listen(port, "127.0.0.1");
